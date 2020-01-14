ae62729794b22de7063ae9e4261f08acbd1683af
OrriePage <57916909+OrriePage@users.noreply.github.com>
2020-01-14 15:15:16 -0500

    adding intial
    adding

---
+++ 2.5-Men/client/src/components/pages/Skeleton.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "../../utilities.css";
+import "./Skeleton.css";
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+
+class Skeleton extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize Default State
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // remember -- api calls go here!
+  }
+
+  render() {
+    return (
+      <>
+        <nav class="nav">
+          <div class = "navtext row">
+          <div className = "row logo"> Logo</div>
+          <div className = "row login">
+          {this.props.userId ? (
+          <GoogleLogout
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={this.props.handleLogout}
+            onFailure={(err) => console.log(err)}
+          />
+        ) : (
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={this.props.handleLogin}
+            onFailure={(err) => console.log(err)}
+          />
+        )}
+        </div>
+        </div>
+        </nav>
+
+        <div class = "u-flex-justifyCenter u-textCenter u-flex-alignCenter">
+          <h1>Login page :)</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Skeleton;
